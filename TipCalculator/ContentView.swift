822b7a73bd1cdbafffcc30a7fd659c3c8db7bc94
Alejandro Franco <jafrancov@gmail.com>
2020-02-28 22:52:56 -0600

    Tip calculator

--- TipCalculator/ContentView.swift
+++ TipCalculator/ContentView.swift
@@ -9,9 +9,51 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    @State private var totalCuenta = ""
+    @State private var numeroPersonas = 2
+    @State private var porcentajePropina = 1
+    
+    let porcentajesPropina = [0, 10, 15, 20, 25]
+    
+    var totalPorPersona: Double {
+        let nPersonas = Double(numeroPersonas + 2)
+        let pPropina = Double(porcentajesPropina[porcentajePropina]) / 100.00
+        let tCuenta = Double(totalCuenta) ?? 0.00
+        return tCuenta * pPropina / nPersonas
     }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Total cuenta", text: $totalCuenta)
+                        .keyboardType(.decimalPad)
+                    Picker("Número de personas", selection: $numeroPersonas) {
+                        ForEach(2 ..< 50) { index in
+                            Text("\(index) personas")
+                        }
+                    }
+                }
+                Section(header: Text("¿Cuánta propina deseas dejar?")) {
+                    Picker("Porcentaje de propina", selection: $porcentajePropina) {
+                        ForEach(0 ..< self.porcentajesPropina.count) {
+                            Text("\(self.porcentajesPropina[$0]) %")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                Section {
+                    Text("$\(totalPorPersona, specifier: "%.2f")")
+                }
+            }.navigationBarTitle("Tip Calculator")
+        }
+    }
+    
+//    func calcular() -> Double {
+//        let nPersonas = Double(numeroPersonas + 2)
+//        let pPropina = Double(porcentajesPropina[porcentajePropina]) / 100.00
+//        let tCuenta = Double(totalCuenta) ?? 0.00
+//        return tCuenta * pPropina / nPersonas
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
